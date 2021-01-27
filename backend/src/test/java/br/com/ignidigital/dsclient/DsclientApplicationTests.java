23213ab446207a41b9b2fc5dba1da0fd516775d6
Jackson Rodrigues <jjr0910@gmail.com>
2021-01-27 00:04:01 -0300

    Project created

---
+++ backend/src/test/java/br/com/ignidigital/dsclient/DsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ignidigital.dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
